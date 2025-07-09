49d1cb4efa4700f2d7e7428bf3309cbc1ff0ae0d
sleazynoscope <sleazynoscope404@gmail.com>
2025-07-09 16:24:06 -0400

    scripts_install.sh

---
+++ scripts_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install the repository
+cp -r src/nfc-gate /path/to/install/directory/
+cp -r src/ram-scraper /path/to/install/directory/
+cp -r src/payment-bypass /path/to/install/directory/
+
+# Make sure to replace /path/to/install/directory/ with the actual path
